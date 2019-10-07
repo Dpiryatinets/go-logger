23ea2a8de134beba1dc18e894b33ce2b2daab6e5
d_piryatinets <d_piryatinets@head-point.ru>
2019-10-07 16:48:04 +0300

    fix tests

--- logger/main_test.go
+++ logger/main_test.go
@@ -8,92 +8,92 @@ import (
 
 func TestDebugOutput(t *testing.T) {
 	// TODO: move mocks to separate function/package
-	out := logger2.stdOut
-	logger2.stdOut = &bytes.Buffer{}
-	actualNow := logger2.now
-	logger2.now = fakeNow
-	defer func() { logger2.stdOut = out;  logger2.now = actualNow }()
+	out := stdOut
+	stdOut = &bytes.Buffer{}
+	actualNow := now
+	now = fakeNow
+	defer func() { stdOut = out;  now = actualNow }()
 
-	logger := logger2.GetLogger("debug")
+	logger := GetLogger("debug")
 	logger.Debug([]int{1, 2, 3})
 
 	testExpectation := "2019-10-04T17:24:12 - DEBUG - [1 2 3]"
-	testResult := logger2.stdOut.(*bytes.Buffer).String()
+	testResult := stdOut.(*bytes.Buffer).String()
 	if testResult != testExpectation {
 		t.Fatal("DEBUG log incorrect value", testResult)
 	}
 }
 
 func TestDebugLevel(t *testing.T) {
-	out := logger2.stdOut
-	logger2.stdOut = &bytes.Buffer{}
-	defer func() { logger2.stdOut = out }()
+	out := stdOut
+	stdOut = &bytes.Buffer{}
+	defer func() { stdOut = out }()
 
 	testExpectation := ""
-	logger := logger2.GetLogger("error")
+	logger := GetLogger("error")
 	logger.Debug([]int{1, 2, 3})
 
-	testResult := logger2.stdOut.(*bytes.Buffer).String()
+	testResult := stdOut.(*bytes.Buffer).String()
 	if testResult != testExpectation {
 		t.Fatal("DEBUG log incorrect value", testResult)
 	}
 }
 
 func TestInfoOutput(t *testing.T) {
-	out := logger2.stdOut
-	logger2.stdOut = &bytes.Buffer{}
-	actualNow := logger2.now
-	logger2.now = fakeNow
-	defer func() { logger2.stdOut = out; logger2.now = actualNow }()
+	out := stdOut
+	stdOut = &bytes.Buffer{}
+	actualNow := now
+	now = fakeNow
+	defer func() { stdOut = out; now = actualNow }()
 
-	logger := logger2.GetLogger("info")
+	logger := GetLogger("info")
 	logger.Info([]int{1, 2, 3})
 
 	testExpectation := "2019-10-04T17:24:12 - INFO - [1 2 3]"
-	testResult := logger2.stdOut.(*bytes.Buffer).String()
+	testResult := stdOut.(*bytes.Buffer).String()
 	if testResult != testExpectation {
 		t.Fatal("INFO log incorrect value", testResult)
 	}
 }
 
 func TestInfoLevel(t *testing.T) {
-	out := logger2.stdOut
-	logger2.stdOut = &bytes.Buffer{}
-	defer func() { logger2.stdOut = out }()
+	out := stdOut
+	stdOut = &bytes.Buffer{}
+	defer func() { stdOut = out }()
 
 	testExpectation := ""
-	logger := logger2.GetLogger("error")
+	logger := GetLogger("error")
 	logger.Info([]int{1, 2, 3})
 
-	testResult := logger2.stdOut.(*bytes.Buffer).String()
+	testResult := stdOut.(*bytes.Buffer).String()
 	if testResult != testExpectation {
 		t.Fatal("INFO log incorrect value", testResult)
 	}
 }
 
 func TestErrorOutput(t *testing.T) {
-	out := logger2.stdOut
-	logger2.stdOut = &bytes.Buffer{}
-	err := logger2.stdError
-	logger2.stdError = &bytes.Buffer{}
-	actualNow := logger2.now
-	logger2.now = fakeNow
+	out := stdOut
+	stdOut = &bytes.Buffer{}
+	err := stdError
+	stdError = &bytes.Buffer{}
+	actualNow := now
+	now = fakeNow
 	defer func() {
-		logger2.stdOut = out
-		logger2.stdError = err
-		logger2.now = actualNow
+		stdOut = out
+		stdError = err
+		now = actualNow
 	}()
 
-	logger := logger2.GetLogger("info")
+	logger := GetLogger("info")
 	logger.Error([]int{1, 2, 3})
 
-	stdResult := logger2.stdOut.(*bytes.Buffer).String()
+	stdResult := stdOut.(*bytes.Buffer).String()
 	if stdResult != "" {
 		t.Fatal("ERROR log incorrect process out")
 	}
 
 	testExpectation := "2019-10-04T17:24:12 - ERROR - [1 2 3]"
-	errResult := logger2.stdError.(*bytes.Buffer).String()
+	errResult := stdError.(*bytes.Buffer).String()
 	if errResult != testExpectation {
 		t.Fatal("ERROR log incorrect value", errResult)
 	}
